98e3bb6bd761f1aabf2b922ff6e9ff90f80a1673
m2bs <intouch@ada-soft.com>
2022-11-10 12:02:44 +0700

    ตั้งค่า URL อัตราแลกเปลี่ยน
    แก้ไขการแสดงชื่อเมนู

---
+++ application/language/en/settingconfig/settingurlformat/settingurlformat_lang.php
@@ -0,0 +1,51 @@
+<?php
+$lang['tUrlName']           = "Details";
+$lang['tUrlCode']           = "Code";
+$lang['tAngName']           = "Agency";
+$lang['tBchName']           = "Branch";
+$lang['tAngDel']            = "Delete";
+$lang['tAngEdit']           = "Edit";
+$lang['tAngTitle']          = "Config URL Exchange Rate";
+$lang['tAngTitleAdd']       = "Add";
+$lang['tAngTitleEdit']      = "Edit";
+$lang['tCountryLang']           = "Language";
+$lang['tCountryLangName']       = "Language";
+$lang['tCountryLangID']         = "Language ID";
+$lang['tAngChoose']             = "Choose";
+$lang['tFmtSta']                = "Status";
+$lang['tFmtStaUse']             = "Use";
+$lang['tFmtStaNotUse']          = "inactive";
+$lang['tNotFound']              = "Item not found";
+
+
+$lang['tFmtStaNotUse']          = "inactive";
+$lang['tAngName']               = "Agency";
+$lang['tSHPValiBranchCode']     = "Please select a branch";
+$lang['tUrlApiValidate']        = "Please select details";
+$lang['tVatTitle']              = "Vat Rate";
+$lang['tVatCode']               = "Vat Code";
+$lang['tCountryLangID']         = "Language ID";
+$lang['tVatStart']              = "Start Date";
+
+$lang['tCountryInfo']               = "general information";
+$lang['tCountryVat']                = "Vat Rate";
+$lang['tCountryIso']                = "ISO Code";
+$lang['tCountryExCtrl']             = "Control Excahnage rate daily";
+$lang['tCountryStaUse']             = "status";
+$lang['tCountryAllow']              = "allow";
+$lang['tCountryNotAllow']           = "not allowed";
+$lang['tCountryUse']                = "enable";
+$lang['tCountryNotUse']             = "disabled";
+$lang['tCountrySearch']             = "Search";
+$lang['tCountryla']                 = "latitude";
+$lang['tCountrylon']                = "longitude";
+
+$lang['tCountryIsoValidate']            = "Please enter the standard currency code. ISO 4217";
+$lang['tCountryNameValidate']           = "Please enter country name";
+$lang['tCountryCodeValidate']           = "Please enter country code eg. ABC";
+$lang['tUrlFormatTitle']                = "URL";
+$lang['tUrlFormatTitleCode']            = "Code";
+$lang['tFormatName']                    = "Choose Format";
+$lang['tCheckSta']                      = "Found active status code รหัส ";
+$lang['tCheckTitle']                    = "activation";
+$lang['tCheckNext']                     = "want to activate URL Format this instead URL original or not";
